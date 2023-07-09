aa9bc3fa735837dd3cc4e74e25de6d1313649d86
boringx <perkeboring@gmail.com>
2023-07-10 00:37:07 +0300

    Create 1) DAO 2) Repository

---
+++ src/main/kotlin/DataRepository.kt
@@ -0,0 +1,8 @@
+class DataRepository() {
+    fun getData(s: String): String {
+        val result = "Hello, World !"
+        // It must be a call to lower interpretation
+        // like to specific DB, or a bit lower, like to DAO
+        return result
+    }
+}
